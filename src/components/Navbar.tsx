4474dbdbc913122e58870181f5b7ea709cd86afe
quangthe0412 <quangthe0412@gmail.com>
2025-10-26 15:45:34 +0700

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,7 +51,7 @@ export const Navbar = () => {
             <div className="w-8 h-8 bg-gradient-primary rounded-lg flex items-center justify-center">
               <ShoppingCart className="w-5 h-5 text-primary-foreground" />
             </div>
-            <span className="font-bold text-xl">ShopVN-Test</span>
+            <span className="font-bold text-xl">ShopVN</span>
           </Link>
 
           {/* Search */}
